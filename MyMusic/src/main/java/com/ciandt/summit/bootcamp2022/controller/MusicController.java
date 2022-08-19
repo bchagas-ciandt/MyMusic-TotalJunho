cf9d0e5cd8cc88e0f856f1d0fd7639e37e1e2769
Eliabe-ciandt <eliabe@ciandt.com>
2022-08-19 12:48:20 -0300

    Removes unused imports

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -1,15 +1,9 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
-import com.ciandt.summit.bootcamp2022.model.Artist;
-import com.ciandt.summit.bootcamp2022.model.Music;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/music")
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/MusicRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/MusicRepository.java
@@ -1,6 +1,5 @@
 package com.ciandt.summit.bootcamp2022.repository;
 
-import com.ciandt.summit.bootcamp2022.model.Artist;
 import com.ciandt.summit.bootcamp2022.model.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
