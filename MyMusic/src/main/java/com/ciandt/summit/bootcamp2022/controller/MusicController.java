95e86eb640abfcbd8753755ca18a3dd351c33863
Eliabe-ciandt <eliabe@ciandt.com>
2022-08-22 12:37:53 -0300

    Criand metodo findByName no Controller

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -1,16 +1,34 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
+import com.ciandt.summit.bootcamp2022.entity.Music;
+import com.ciandt.summit.bootcamp2022.service.MusicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/music")
 public class MusicController {
 
+    @Autowired
+    private MusicService musicService;
+
     @GetMapping
-    public ResponseEntity<String> get() {
+    public ResponseEntity findMusicsByMusicNameOrArtistName(@RequestParam(name = "filtro") String name) {
+
+        List<Music> musics = musicService.findMusicsByMusicNameOrArtistName(name);
+
+        return ResponseEntity.ok(musics);
+
+    }
+
+    @GetMapping
+    public ResponseEntity get() {
         return ResponseEntity.ok("67f5976c-eb1e-404e-8220-2c2a8a23be47");
     }
 
